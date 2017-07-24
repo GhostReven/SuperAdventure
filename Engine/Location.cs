c5bc7dea2f6abfab28adcfd4543bd13dc0c157f8
Peter G Laursen <ghostreven@gmail.com>
2017-07-24 21:38:35 +0200

    Lesson 08.1 Added a constructor to Location and Quest. Plus added a "Home" location, in the constructor for the SuperAdventure form.

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -5,9 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine {
-	class Location {
+	public class Location {
 		public int ID { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
+
+		public Location(int id, string name, string description) {
+			ID = id;
+			Name = name;
+			Description = description;
+		}
 	}
 }
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -18,6 +18,7 @@ namespace SuperAdventure {
 			InitializeComponent();
 
 			_player = new Player();
+			Location location = new Location(1, "Home", "This is your house");
 
 			_player.CurrentHitPoints = 10;
 			_player.MaximumHitPoints = 10;
